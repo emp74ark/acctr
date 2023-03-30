cc663936c26f8e26259fb572d002593f417b5bef
emp74ark <emp74ark@gmail.com>
2023-03-30 23:19:00 +0200

    feat: records list

--- src/pages/Records/Records.tsx
+++ src/pages/Records/Records.tsx
@@ -1,5 +1,35 @@
+import style from './style.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { removeRecord } from '../../store/recordsSlice';
+import { IRecord } from '../../entities';
+import { useState } from 'react';
+
 export const Records = () => {
+  const { records } = useSelector((state: RootState) => state.records);
+  const dispatch = useDispatch();
+  const [editable, setEditable] = useState<{id: number; editable: boolean}>();
+
+  const onRemove = (id: number) => {
+    dispatch(removeRecord(id));
+  };
+
   return (
-      <><h2>Records</h2></>
+      <>
+        <h2>Records</h2>
+        <div className={ style.records }>
+          {
+            records.map(({ id, label, amount, date, tags }) => (
+                <div key={ id } className={ style.record }>
+                  <span>{ date.toISOString().slice(0,10) }</span>
+                  <span>{ label }</span>
+                  <span>{ amount }</span>
+                  <span>{ tags.join(', ') }</span>
+                  <button onClick={ () => onRemove(id) }>Remove</button>
+                </div>
+            ))
+          }
+        </div>
+      </>
   );
 };
