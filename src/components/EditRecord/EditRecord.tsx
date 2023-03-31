b26012c69142cac19d7ce6de2331e95ede390901
emp74ark <emp74ark@gmail.com>
2023-03-31 12:34:52 +0200

    fix: return EditRecord back

---
+++ src/components/EditRecord/EditRecord.tsx
@@ -0,0 +1,54 @@
+import style from './style.module.css';
+import { IRecord } from '../../entities';
+import { useDispatch } from 'react-redux';
+import { editRecord } from '../../store/recordsSlice';
+import { ChangeEvent, useState } from 'react';
+
+export const EditRecord = ({ record, cb }: { record: IRecord, cb: () => void }) => {
+  const { label, amount } = record;
+  const dispatch = useDispatch();
+  const [field, setField] = useState<Record<string, string | number>>();
+
+  const onSave = () => {
+    if (field) {
+      dispatch(editRecord({
+        ...record,
+        ...field,
+      }));
+      cb()
+    }
+  };
+
+  const onCancel = () => {
+    cb();
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>, name: string) => {
+    setField({
+      [name]: e.currentTarget.value,
+    });
+  };
+
+  return (
+      <div className={ style.wrapper }>
+        <div className={ style.editor }>
+          <label>Label
+            <input
+                type="text"
+                defaultValue={ label }
+                onChange={ (e) => onChange(e, 'label') }
+            />
+          </label>
+          <label>Amount
+            <input
+                type="text"
+                defaultValue={ amount }
+                onChange={ (e) => onChange(e, 'amount') }
+            />
+          </label>
+          <button disabled={!field} onClick={ onSave }>Save</button>
+          <button onClick={ onCancel }>Cancel</button>
+        </div>
+      </div>
+  );
+};
