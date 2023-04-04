f2ac7774e1d450ecd4622a768a7d3265a69d301b
emp74ark <emp74ark@gmail.com>
2023-04-04 21:06:55 +0200

    feat: save data to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import { Home, Records, Statistics } from './pages';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import { Header } from './components';
+import React, {useEffect} from 'react';
+import {Home, Records, Statistics} from './pages';
+import {BrowserRouter, Outlet, Route, Routes} from 'react-router-dom';
+import {Header} from './components';
+import {useSelector} from "react-redux";
+import {RootState} from "./store/store";
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/records' element={<Records/>}/>
-          <Route path='/statistics' element={<Statistics/>}/>
-        </Routes>
-        <Outlet/>
-      </BrowserRouter>
-  );
+    const {records: {records, tags}, groups: {groups}} = useSelector((state: RootState) => state)
+
+    useEffect(() => {
+        localStorage.setItem('acctr', JSON.stringify({records, tags, groups}))
+    }, [records, groups]);
+
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/records' element={<Records/>}/>
+                <Route path='/statistics' element={<Statistics/>}/>
+            </Routes>
+            <Outlet/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
