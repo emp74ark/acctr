aa2c32f6bc2da764a24cd0c33f0711638a918009
emp74ark <emp74ark@gmail.com>
2023-04-04 19:08:30 +0200

    feat: pie chart

--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -6,6 +6,11 @@ import {getAmountByTag} from "../../utils";
 import {RootState} from "../../store/store";
 import {useState} from "react";
 import {GroupEditor} from "../GroupEditor";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import * as uniqolor from "uniqolor";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const Group = (group: IGroup) => {
     const {records} = useSelector((state: RootState) => state.records)
@@ -20,6 +25,19 @@ export const Group = (group: IGroup) => {
         setEdit(!edit)
     };
 
+    const data = {
+        labels: group.tags,
+        datasets: [
+            {
+                label: 'amount',
+                data: group.tags?.map(tag => getAmountByTag(tag, records)),
+                backgroundColor: group.tags?.map(tag => uniqolor.random().color),
+                borderColor: group.tags?.map(tag => '#fff'),
+                borderWidth: 3,
+            },
+        ],
+    };
+
     return (
         <div className={style.group}>
             <h4>{group.name}</h4>
@@ -30,6 +48,7 @@ export const Group = (group: IGroup) => {
                     ))}
                 </ul>
             </div>
+            <Pie data={data}/>
             <button onClick={onRemove}>Remove</button>
             <button onClick={onEdit}>Edit</button>
             {edit && <GroupEditor type='edit' cancel={onEdit} group={group}/>}
