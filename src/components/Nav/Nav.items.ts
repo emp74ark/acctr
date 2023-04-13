1bc7538aeeb875705d1f9369e752d71d54dfd5d4
emp74ark <emp74ark@gmail.com>
2023-04-13 23:29:22 +0200

    feat: burger + mobile menu

---
+++ src/components/Nav/Nav.items.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "records",
+    path: "/records",
+  },
+  {
+    name: "statistics",
+    path: "/statistics",
+  },
+]
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,39 @@
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import style from "./style.module.css";
+import {useState} from "react";
+import {navItems} from "./Nav.items";
 
 export const Nav = () => {
+  const [burger, setBurger] = useState<boolean>(false);
+
+  const onBurger = () => {
+    setBurger(!burger)
+  }
+
   return (
       <nav className={style.nav}>
         <ul>
-          <li><NavLink to="/">home</NavLink></li>
-          <li><NavLink to="/records">records</NavLink></li>
-          <li><NavLink to="/statistics">statistics</NavLink></li>
+          {navItems.map(({name, path}) =>
+              <li key={name}><NavLink to={path}>{name}</NavLink></li>
+          )}
         </ul>
+        <div
+            onClick={onBurger}
+            className={burger ? `${style.burger} ${style.burger_active}` : `${style.burger}`}>
+          <div className={style.burger__line}></div>
+          <div className={style.burger__line}></div>
+          <div className={style.burger__line}></div>
+        </div>
+        <div
+            onClick={onBurger}
+            className={burger ? `${style.mobile__nav} ${style.mobile__nav_active}` : `${style.mobile__nav}`}>
+          <ul>
+            {navItems.map(({name, path}) =>
+                <li key={name}><NavLink to={path}>{name}</NavLink></li>
+            )}
+          </ul>
+        </div>
+        {burger && <div onClick={onBurger} className="shadow"/>}
       </nav>
   );
 };
