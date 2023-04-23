fdad4c61c24bcb97c6bc1767d5a36b9ea4310335
emp74ark <emp74ark@gmail.com>
2023-04-23 14:34:55 +0200

    feat: add i18n support

---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import en from "./assets/i18n/en.json";
+import ru from "./assets/i18n/ru.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  ru: {
+    translation: ru,
+  }
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  react: {
+    transEmptyNodeValue: "",
+    transSupportBasicHtmlNodes: true,
+    transKeepBasicHtmlNodesFor: ["br", "strong", "i", "p"]
+  }
+});
+
+export default i18n;
